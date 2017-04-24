2d19565597743a2faead3de3c98ddf9de60205d1
MarcelGar6 <marcel.m.garcez@gmail.com>
2017-04-24 00:01:29 -0300

    Lesson 13 finished

---
+++ EcommerceApplication/EcommerceApplication/Services/Infrastructure/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.Services.Infrastructure
+{
+    public interface ICategory
+    {
+        IEnumerable<Category> GetAll();
+        Category GetById(int id);
+        void Insert(Category cat);
+        void Update(Category cat);
+        void Delete(int id);
+
+    }
+}
